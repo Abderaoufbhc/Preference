731dca8b8871fd61ae342f4eb687f6b778b5a94b
Yinan Fang <yinan_fang@hotmail.com>
2016-08-13 16:11:56 -0700

    Include Atom settings. Add path checks

--- Mac/gather_settings.sh
+++
@@ -1,5 +0,0 @@
-rsync -u --progress ~/.zshrc .
-rsync -u --progress ~/.tarc .
-rsync -u --progress ~/.vimrc .
-rsync -u --progress ~/.gitconfig .
-rsync -u --progress ~/.tmux.conf .
---
+++ gather_settings.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Format: rsync -u --progress ~/.zshrc ./Mac
+
+# Shell config
+echo 'Gathering Shell config...'
+rsync -uq ~/.zshrc ./Mac
+rsync -uq ~/.vimrc ./Mac
+
+if [ -f '~/.tarc' ]; then
+  echo 'Gathering TripAdvisor Shell config...'
+  rsync -uq ~/.tarc ./Mac
+fi
+
+# Git
+echo 'Gathering Git config...'
+rsync -uq ~/.gitconfig ./Mac
+
+# Tmux
+if [ -f '~/.tmux.conf' ]; then
+  echo 'Gathering Tmux config...'
+  rsync -uq ~/.tmux.conf ./Mac
+fi
+
+# Atom
+if hash atom 2>/dev/null; then
+  echo 'Gathering Atom Community Packages...'
+  mkdir -pv ./Mac/.atom
+  apm list --installed --bare > ~/Code/Preference/Mac/.atom/package-list.txt
+  rsync -uq ~/.atom/config.cson ./Atom
+else
+  echo 'Atom not installed!'
+fi
+
+echo 'Done!'
