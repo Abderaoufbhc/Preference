5ca55f1623e28485c1361764c23d2149e5433b4a
Yinan Fang <yinanfang@gmail.com>
2017-05-15 14:36:13 -0700

    Copy the tmuxinator setting

--- update_local_settings.sh
+++ update_local_settings.sh
@@ -7,6 +7,11 @@ curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.tarc > ~
 curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.gitconfig > ~/.gitconfig
 
 curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.tmux.conf > ~/.tmux.conf
+# Make sure .tmuxinator exists
+if [ ! -d ~/.tmuxinator ]; then
+  mkdir ~/.tmuxinator
+fi
+curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.tmuxinator/trip.yml > ~/.tmuxinator/trip.ymp
 
 curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.atom/package-list.txt > ~/.atom/package-list.txt
 curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.atom/config.cson > ~/.atom/config.cson
