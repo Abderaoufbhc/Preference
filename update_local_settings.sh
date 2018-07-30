2ef4bd62854b7a5216c1bdf70c022fbd31dc925f
Lucas Fang <yfang@snapchat.com>
2018-07-30 12:56:35 -0700

    update

--- update_local_settings.sh
+++ update_local_settings.sh
@@ -5,7 +5,7 @@ curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.tarc > ~
 curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.snaprc > ~/.snaprc
 
 curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.vimrc > ~/.vimrc
-if [ ! -d ~/.config/ ]; then
+if [ ! -d ~/.config/nvim ]; then
   mkdir -pv ~/.config/nvim
 fi
 curl https://raw.githubusercontent.com/yinanfang/Preference/master/Mac/.config/nvim/init.vim > ~/.config/nvim/init.vim
